a9b385f580f832635632fd81894c4d24bf8dee7c
Beehrad <beehrad@yahoo.com>
2023-11-23 17:34:28 +0100

    API conected

--- 2- Shopping/app.js
+++ 2- Shopping/app.js
@@ -1,7 +1,9 @@
+
 const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const card = document.querySelector(".fa-cart-shopping");
 const confirmCard = document.querySelector(".confirm")
+const products = document.querySelector(".products")
 
 confirmCard.addEventListener("click", closeModal);
 
@@ -17,3 +19,30 @@ function closeModal() {
   modal.style.opacity = "0";
   modal.style.transform = "translateY(-100vh)";
 }
+
+
+function onLoad(){
+  let UI = ""
+  fetch("https://fakestoreapi.com/products")
+  .then((response) => response.json())
+  .then((element) =>{
+    element.forEach(e => {
+      console.log(e)
+      UI += `
+      <div class="item">
+        <h6>${e.title}</h6>
+        <img src="${e.image}" alt="">
+        <div>
+            <p>${e.price}</p>
+            <p>${e.category}</p>
+        </div>
+        <button class="add-to-cart">add to cart</button>
+      </div>
+      `
+    });
+    products.innerHTML = UI
+  })
+
+}
+
+onLoad()
