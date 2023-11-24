fbeb99b134b73abcbd4694dfba98f4c294a4c464
Beehrad <beehrad@yahoo.com>
2023-11-24 17:39:29 +0100

    Details product by Click

--- 2- Shopping/app.js
+++ 2- Shopping/app.js
@@ -3,11 +3,14 @@ const modal = document.querySelector(".modal");
 const card = document.querySelector(".fa-cart-shopping");
 const confirmCard = document.querySelector(".confirm")
 const products = document.querySelector(".products")
+const sub = document.querySelector(".addtocart")
 
 confirmCard.addEventListener("click", closeModal);
 
+
 card.addEventListener("click", showModal);
 document.addEventListener("DOMContentLoaded", onLoad)
+products.addEventListener("click" , addToCard)
 
 function showModal() {
   backdrop.style.display = "flex";
@@ -26,19 +29,33 @@ function onLoad(){
   .then((response) => response.json())
   .then((element) =>{
     element.forEach(e => {
-      console.log(e)
       UI += `
       <div class="item">
         <h6>${e.title}</h6>
         <img src="${e.image}" alt="">
         <div>
-            <p>${e.price}$</p>
+            <span value="${e.price}">${e.price}$</span>
             <p>${e.category}</p>
         </div>
-        <button class="add-to-cart">add to cart</button>
+        <button class="addtocart">add to cart</button>
       </div>
       `
     });
     products.innerHTML = UI
   })
+}
+
+function addToCard(e){
+  // console.log(e.target)
+  let target = e.target
+  let product = {}
+  if(target.className == "addtocart"){
+    let parent = target.parentElement
+    console.log(parent.children[2].children[0].textContent.split("$")[0])
+    product.title = parent.children[0].innerHTML
+    product.image = parent.children[1]
+    product.price = parent.children[2].children[0].textContent.split("$")[0]
+
+    return product
+  }
 }
