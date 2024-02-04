e11dfb2983368c8330b53727ea6d7361603c75dd
Beehrad <beehrad@yahoo.com>
2024-02-04 12:17:09 +0100

    useReducer Added

--- 4- React Turtial/src/App.jsx
+++ 4- React Turtial/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import Search from "./components/search";
 import Stories from "./components/Stories";
 import Name from "./components/Name";
@@ -6,9 +6,18 @@ import useLocalHook from "./components/hooks/useLocalHook";
 
 const App = () => {
   const [name, setName] = useState("Behrad");
-  const [searchTerm, setSearchTerm] = useLocalHook("search" , '')
+  const [searchTerm, setSearchTerm] = useLocalHook("search", "");
 
-  const [story, setStory] = useState([
+  const storiesReducer = (state, action) => {
+    switch (action.type) {
+      case "SET_STORY":
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+
+  const [story, dispatchedStories] = useReducer(storiesReducer, [
     {
       id: 1,
       title: "car",
@@ -30,6 +39,28 @@ const App = () => {
       city: "booshehr",
     },
   ]);
+  // const [story, setStory] = useState([
+  //   {
+  //     id: 1,
+  //     title: "car",
+  //     city: "bandar",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "maschin",
+  //     city: "tehran",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "wohnung",
+  //     city: "ahvaz",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "motor",
+  //     city: "booshehr",
+  //   },
+  // ]);
 
   const searchStories = story.filter((item) => {
     if (item.title.includes(searchTerm)) {
@@ -37,17 +68,23 @@ const App = () => {
     }
   });
 
-  const removeStory = (id)=>{
-    const neuStories = story.filter((item)=> item.id !== id)
-    setStory(neuStories)
-  }
+  const removeStory = (id) => {
+    const neuStories = story.filter((item) => item.id !== id);
+    // setStory(neuStories);
+    dispatchedStories({type:"SET_STORY" , payload: neuStories})
+  };
 
   return (
     <div>
       <h1>Hello {name}</h1>
       <Name setName={setName} />
-      <Search id="search" label="Search" setSearchTerm={setSearchTerm} value={searchTerm}/>
-      <Stories searchStories={searchStories} removeStory={removeStory}/>
+      <Search
+        id="search"
+        label="Search"
+        setSearchTerm={setSearchTerm}
+        value={searchTerm}
+      />
+      <Stories searchStories={searchStories} removeStory={removeStory} />
     </div>
   );
 };
